bfe9dff08c6002954a614e69731e3e7e613f2a7b
Tre Island <t.island84@gmail.com>
2022-01-27 18:12:24 -0700

    test set card value

--- tests/test_game.py
+++ tests/test_game.py
@@ -5,6 +5,7 @@ class TestGame:
     def setup_class(cls):
         pass
 
+    
     def test_create_card(self):
         rank="ace"
         suit="clubs"
@@ -16,3 +17,14 @@ class TestGame:
         assert(card.suit==suit)
         assert(str(card) == f"{rank} of {suit}")
 
+    def test_set_card_value(self):
+        rank="5"
+        suit="clubs"
+
+        card = Card(rank, suit)
+
+        assert(card.value == 0)
+
+        card.value=5
+
+        assert(card.value == 5)
